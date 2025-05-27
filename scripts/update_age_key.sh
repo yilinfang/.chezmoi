c5e9cc16676454b4e1466990af752fe4713853c3
Yilin Fang <qzfyl98@outlook.com>
2025-05-27 02:21:25 -0400

    age: Rotate key

--- scripts/update_age_key.sh
+++ scripts/update_age_key.sh
@@ -3,6 +3,10 @@
 # Remove current age key if it exists
 chezmoi forget ~/.age
 
+# Remove the old age key file if it exists
+rm -rf ~/.age
+mkdir -p ~/.age
+
 # Generate a new age key and save it to ~/.age/key.txt.age
 age-keygen | age -a -p >~/.age/key.txt.age
 

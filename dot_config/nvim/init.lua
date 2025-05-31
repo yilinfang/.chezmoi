73b48df1cce23d0d77035cc71e1d7f2a82ba7e42
Yilin Fang <qzfyl98@outlook.com>
2025-05-31 15:10:51 -0400

    nvim: Switch to gitsigns, lualine and nvim-devicons

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -193,8 +193,73 @@ require('lazy').setup({
     end,
   },
 
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    event = { unpack(LazyFile) }, -- HACK: Set the event of gitsigns.nvim to LazyFile
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr) -- HACK: Additional keymaps for gitsigns
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+
+        -- Actions
+        -- visual mode
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'git [s]tage hunk' })
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'git [r]eset hunk' })
+        -- normal mode
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+        map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis '@'
+        end, { desc = 'git [D]iff against last commit' })
+        -- Toggles
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+        map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+      end,
+    },
+  },
+
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- HACK: nvim-web-devicons for which-key.nvim
     event = 'VeryLazy', -- HACK: Set the event of which-key.nvim to VeryLazy
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
@@ -222,6 +287,10 @@ require('lazy').setup({
 
   { -- HACK: Snacks.picker
     'folke/snacks.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
     opts = {
       picker = { enabled = true },
       image = {
@@ -764,23 +833,8 @@ require('lazy').setup({
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       require('mini.surround').setup()
 
-      -- HACK: Customized mini.statusline
-      require('mini.statusline').setup { use_icon = true }
-
       -- HACK: Other Mini modules
       require('mini.comment').setup()
-      require('mini.diff').setup {
-        view = {
-          style = 'sign',
-          signs = {
-            add = '▎',
-            change = '▎',
-            delete = '',
-          },
-        },
-      }
-      require('mini.git').setup()
-      require('mini.icons').setup()
       require('mini.move').setup()
       require('mini.pairs').setup()
     end,
@@ -851,6 +905,49 @@ require('lazy').setup({
     end,
   },
 
+  { -- HACK: lualine.nvim for statusline
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'solarized-osaka',
+          always_show_tabline = false,
+          disabled_filetypes = { statusline = { 'snacks_dashboard' } },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch' },
+          lualine_c = {
+            { 'filename', path = 1 }, -- Relative path
+            {
+              'diff',
+              source = function() -- Use gitsigns.nvim for diff
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_x = {
+            'diagnostics',
+            'filetype',
+            { 'encoding', show_bomb = true },
+          },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        extensions = { 'lazy', 'mason', 'aerial', 'quickfix' },
+      }
+    end,
+  },
+
   { -- HACK: Other snacks.nvim modules
     'folke/snacks.nvim',
     priority = 1000,
@@ -885,7 +982,7 @@ require('lazy').setup({
 
   { -- HACK: render-markdown.nvim
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     ft = { 'markdown', 'norg', 'rmd', 'org', 'codecompanion' },
     opts = {
       code = {
@@ -947,7 +1044,7 @@ require('lazy').setup({
     -- Optional dependencies
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
-      'echasnovski/mini.nvim',
+      'nvim-tree/nvim-web-devicons',
     },
     opts = {
       layout = {

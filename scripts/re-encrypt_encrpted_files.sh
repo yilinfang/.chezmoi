caa1c99ee969a191dd15f26106fc4bdd0b556358
Yilin Fang <qzfyl98@outlook.com>
2025-06-10 01:12:16 -0400

    chezmoi: Fix permissons of encrypted files

--- scripts/re-encrypt_encrpted_files.sh
+++ scripts/re-encrypt_encrpted_files.sh
@@ -7,5 +7,8 @@ for encrypted_file in $(chezmoi managed --include encrypted --path-style absolut
 	# strip the .asc extension
 	decrypted_file="${encrypted_file%.asc}"
 
+	# Set the permissions of the decrypted file to read/write for the user only
+	chmod 600 "$decrypted_file"
+
 	chezmoi add --encrypt "$decrypted_file"
 done
